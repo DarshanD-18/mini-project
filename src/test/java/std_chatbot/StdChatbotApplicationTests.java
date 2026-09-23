650de48966a648a1149c672308f770c99d7877b6
Darshan D <darshan@darshandghl@gmail.com>
2026-09-23 23:18:07 +0530

    Set up Spring Boot frontend foundation

---
+++ src/test/java/std_chatbot/StdChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package std_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
